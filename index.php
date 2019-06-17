0a4289442362d0095ad94014601f764388583aa1
Paul Colța <coltapaulstefan@gmail.com>
2019-06-17 15:03:42 +0300

    Updates

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <title> Sign Up </title>
+      <title> Memes Newsletter </title>
+      <link rel="shortcut icon" type="image/x-icon" href="iconita.png" />
   </head>
   <link rel="stylesheet" type="text/css" href="style.css">
   <body>
     <div class="form-wrap">
       <form method="POST" action="signup.php">
-          <h1>Sign Up</h1>
+          <h1>Memes Newsletter</h1>
           <input type="text" name="FirstName" placeholder="First Name">
           <input type="text" name="LastName" placeholder="Last Name">
           <input type="number" name="Age" placeholder="Age(For NSFW Memes)">
