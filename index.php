e4bb8e08f91c9692a1fdf354f7acc9eee203e8d7
Paul Colța <coltapaulstefan@gmail.com>
2019-06-17 14:23:02 +0300

    Style.css repair

--- index.php
+++ index.php
@@ -3,31 +3,7 @@
   <head>
       <title> Sign Up </title>
   </head>
-  <style>
-    {
-      margin: 0;
-      padding: 0;
-    }
-    body {
-      background-image: url("bg.jpg");
-        }
-    .form-wrap{
-      width: 320px; background: #8c8c8c; padding: 40px 20px; box-sizing: border-box;
-      position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%);}
-    h1{
-      text-align: center; color:#fff; font-weight: normal;
-      margin-bottom: 20px;
-      }
-    input{
-      width: 100%; background: none; border: 1px solid #fff; border-radius: 3px; padding: 6px 20px;
-      box-sizing: border-box; margin-bottom: 20px;
-      font-size: 20px;
-    }
-    input[type="submit"]{background: #2db300; border: 0; cursor: pointer; color: #000000;
-    }
-    input[type="submit"]:hover{background:#39e600; transition: .3s;}
-    ::placeholder{color: #fff;}
-  </style>
+  <link rel="stylesheet" type="text/css" href="style.css">
   <body>
     <div class="form-wrap">
       <form method="POST" action="signup.php">
