990d20d89238ca33a0a041ff583485fb63553591
DianaElenaStancu <41987690+DianaElenaStancu@users.noreply.github.com>
2019-06-18 14:31:49 +0300

    make it ugly :(

--- index.php
+++ index.php
@@ -7,42 +7,9 @@
   <head>
       <title> Memes Newsletter </title>
       <link rel="shortcut icon" type="image/x-icon" href="iconita.png" />
+      <link rel="stylesheet" type="text/css" href="style.css">
   </head>
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <style>
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    width: 200px;
-    background-color: #f1f1f1;
-  }
-
-  li a {
-    display: block;
-    color: #000;
-    transition: .3s;
-    padding: 8px 16px;
-    text-decoration: none;
-  }
-
-  li a.active {
-    background-color:#3365ff  ;
-    color: white;
-  }
-
-  li a:hover:not(.active) {
-    background-color: #555;
-    color: white;
-  }
-  </style>
   <body>
-    <header>
-      <ul>
-        <li><a class="active" href="index.php">Sign Up</a></li>
-        <li><a href="contact.html">Contact</a></li>
-      </ul>
-    </header>
     <div class="form-wrap">
       <form method="POST" action="signup.php">
           <h1>Memes Newsletter</h1>
@@ -51,7 +18,8 @@
           <input type="number" name="Age" placeholder="Age(For NSFW Memes)">
           <input type="email" name="email" placeholder="Email">
           <input type="submit" value="Sign Up">
-       </form>
+          <a href = "contact.html"><input type = "contact" value="Contact Us"></a>
+      </form>
     </div>
   </body>
 </html>
